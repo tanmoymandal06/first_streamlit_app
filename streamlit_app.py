e756577e66718b764450a90c9d726bd1b878202a
tanmoymandal06 <149082884+tanmoymandal06@users.noreply.github.com>
2023-10-26 22:46:26 +0530

    Update streamlit_app.py
    update title and add  another header with image

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 
@@ -7,3 +7,5 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.title('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
