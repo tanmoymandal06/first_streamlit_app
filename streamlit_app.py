848f4295d70f570269da4aff5651a1947a059b59
tanmoymandal06 <149082884+tanmoymandal06@users.noreply.github.com>
2023-11-03 19:48:59 +0530

    Update streamlit_app.py
    give choice for choose multiple  fruits at a time

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,14 +58,15 @@ def get_fruit_load_list():
 if streamlit.button('Get Fruit Load List'):
    my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
    my_data_rows = get_fruit_load_list()
+   my_cnx.close()
    streamlit.dataframe(my_data_rows)
 
 #streamlit.stop()
 #allow the end user to add a fruit to the list
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values('from streamlit')")
-        return"Thanks for adding "+ new_fruit
+        my_cur.execute("insert into fruit_load_list values('" + new_fruit +"')")
+        return f"Thanks for adding+" "+{new_fruit}"
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
